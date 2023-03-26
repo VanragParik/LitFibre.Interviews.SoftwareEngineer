411603a3928a41c70656a473065c61fdcb02bdee
vanrag <pareekv@tuckerssolicitors.com>
2023-03-26 19:16:28 +0100

    First Commit - Created new ApiResponse Class

---
+++ LitFibre.Interviews.SoftwareEngineer.Models/Orders/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LitFibre.Interviews.SoftwareEngineer.Models.Orders
+{
+    public class ApiResponse
+    {
+        public string? Status { get; set; }
+        public string? Code { get; set; }
+        public string? Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public object? Data { get; set; }
+    }
+}
